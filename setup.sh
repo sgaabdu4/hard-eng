5efd4e4dfba62986c704809672cdfde731b30ed2
sgaabdu4 <abdulgafoorabid@gmail.com>
2026-09-09 06:23:11 +0100

    Refactor code structure for improved readability and maintainability

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -eu
-hard_eng_download=$(mktemp -d)
-trap 'rm -rf "$hard_eng_download"' 0
-git clone --quiet --filter=blob:none https://github.com/sgaabdu4/hard-eng.git "$hard_eng_download"
-python3 "$hard_eng_download/bin/hard-eng" install "$@"
